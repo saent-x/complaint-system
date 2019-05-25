c1217cecaa0ceac06c1a35dce4222831cbd07c37
Vangerwua Johnpaul <torsxperience@outlook.com>
2019-05-25 19:16:17 +0100

    deleted files

--- packages/backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-
-// Initializations
-const app = express();
