72e7b2f31e5ee52a0b39516f596ae2699caafab8
Torsxperience <Torsxperience@outlook.com>
2019-06-12 23:50:02 +0100

    added joi validation function

--- packages/backend/models/staff.js
+++ packages/backend/models/staff.js
@@ -1,14 +1,32 @@
-const mongoose = require('mongoose');
-const { ComplaintSchema } = require('./complaint');
+const mongoose = require("mongoose");
+const { ComplaintSchema } = require("./complaint");
+const Joi = require("joi");
 
 const StaffSchema = new mongoose.Schema({
-    Name: String,
-    StaffNo: String,
-    ManagingDepartment: String,
+	Name: String,
+	StaffNo: String,
+	ManagingDepartment: String,
     EmailAddress: String,
-    AssignedComplaints: [ mongoose.Schema(ComplaintSchema) /* Format for exported schemas */]
+    Password: String,
+	AssignedComplaints: [
+		mongoose.Schema(ComplaintSchema) /* Format for exported schemas */
+	]
 });
+
+function ValidateStaff(staff) {
+	const schema = {
+        Name: Joi.string().required(),
+        Password: Joi.string().min(8).required(),
+		StaffNo: Joi.string().required(),
+		ManagingDepartment: Joi.string().required(),
+		EmailAddress: Joi.string()
+			.email()
+			.required()
+	};
+
+	return Joi.validate(staff, schema);
+}
 
 const Staff = mongoose.model("Staff", StaffSchema);
 
-module.exports = { Staff, StaffSchema };
+module.exports = { Staff, StaffSchema, ValidateStaff };
--- packages/backend/models/student.js
+++ packages/backend/models/student.js
@@ -3,13 +3,14 @@ const { ComplaintSchema } = require("./complaint");
 const Joi = require("joi");
 
 const StudentSchema = {
-  Name: String,
-  EmailAddress: String,
-  MatNo: String,
-  Department: String,
-  Level: String,
-  CourseOfStudy: String,
-  Complaints: [mongoose.Schema(ComplaintSchema)]
+	Name: String,
+	EmailAddress: String,
+	Password: String,
+	MatNo: String,
+	Department: String,
+	Level: String,
+	CourseOfStudy: String,
+	Complaints: [mongoose.Schema(ComplaintSchema)]
 };
 
 const Student = mongoose.model("Student", StudentSchema);
@@ -17,20 +18,23 @@ const Student = mongoose.model("Student", StudentSchema);
 // Validation function
 
 function ValidateStudent(student) {
-  const schema = {
-    Name: Joi.string().required(),
-    EmailAddress: Joi.string()
-      .email()
-      .required(),
-    MatNo: Joi.string().required(),
-    Department: Joi.string(),
-    Level: Joi.string().required(),
-    CourseOfStudy: Joi.string().required(),
-    Complaints: Joi.string().required()
-  };
+	const schema = {
+		Name: Joi.string().required(),
+		EmailAddress: Joi.string()
+			.email()
+			.required(),
+		Password: Joi.string()
+			.min(8)
+			.required(),
+		MatNo: Joi.string().required(),
+		Department: Joi.string(),
+		Level: Joi.string().required(),
+		CourseOfStudy: Joi.string().required(),
+		Complaints: Joi.string().required()
+	};
 
-  return Joi.validate(student, schema);
+	return Joi.validate(student, schema);
 }
 
 //Exports
-module.exports = { Student, StudentSchema };
+module.exports = { Student, StudentSchema, ValidateStudent };
