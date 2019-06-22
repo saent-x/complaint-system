54d8efb8fe1d173178143b7df6e9af88c8083334
Torsxperience <Torsxperience@outlook.com>
2019-06-22 14:40:31 +0100

    Made little adjustments

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const authentication = require('./routes/authentication');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const authentication = require("./routes/authentication");
 
 // Connect Database
-mongoose.connect("mongodb://localhost/aui-cms", {
-    useNewUrlParser: true
-})
-    .then(() => console.log("db connected."))
-    .catch((err) => console.log(err));
+mongoose
+	.connect("mongodb://localhost/aui-cms", {
+		useNewUrlParser: true
+	})
+	.then(() => console.log("db connected."))
+	.catch(err => console.log(err));
 
 // Initializations
 const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+
 // Middleware Routes
-app.use('/api/auth', authentication)
+app.use("/api/auth", authentication);
 
 const PORT = process.env.PORT || 5000;
 
