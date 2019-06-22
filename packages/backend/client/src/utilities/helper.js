e6fb8964c1bce1838cace2ddac3aa4848df41182
Torsxperience <Torsxperience@outlook.com>
2019-06-22 14:40:44 +0100

    Added helper functions

--- packages/backend/client/src/utilities/helper.js
+++ packages/backend/client/src/utilities/helper.js
@@ -1,2 +1,14 @@
+import jwt from 'jsonwebtoken';
 
 export const API_URL = "http://localhost:5000";
+
+export function StoreToken(token) {
+    localStorage.setItem("token", token);
+}
+
+export function GetTokenInfo(token) {
+    console.log(token);
+    const data = jwt.decode(token);
+    console.log(data);
+    return data;
+}
