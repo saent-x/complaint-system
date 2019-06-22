0eb594c70bc2555a2e9cfbbb522fa08d59070c76
Torsxperience <Torsxperience@outlook.com>
2019-06-22 14:41:23 +0100

    Modified complaints references

--- packages/backend/models/staff.js
+++ packages/backend/models/staff.js
@@ -1,24 +1,23 @@
 const mongoose = require("mongoose");
-const { ComplaintSchema } = require("./complaint");
 const Joi = require("joi");
 
 const StaffSchema = new mongoose.Schema({
 	Name: String,
 	StaffNo: String,
-	ManagingDepartment: String,
-    EmailAddress: String,
-    Password: String,
-	AssignedComplaints: [
-		mongoose.Schema(ComplaintSchema) /* Format for exported schemas */
-	]
+	Department: String,
+	EmailAddress: String,
+	Password: String,
+	AssignedComplaints: [{ type: mongoose.Schema.Types.ObjectId, ref: "Complaint" }]
 });
 
 function ValidateStaff(staff) {
 	const schema = {
-        Name: Joi.string().required(),
-        Password: Joi.string().min(8).required(),
+		Name: Joi.string().required(),
+		Password: Joi.string()
+			.min(8)
+			.required(),
 		StaffNo: Joi.string().required(),
-		ManagingDepartment: Joi.string().required(),
+		Department: Joi.string().required(),
 		EmailAddress: Joi.string()
 			.email()
 			.required()
--- packages/backend/models/student.js
+++ packages/backend/models/student.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { ComplaintSchema } = require("./complaint");
 const Joi = require("joi");
 
 const StudentSchema = {
@@ -10,12 +9,10 @@ const StudentSchema = {
 	Department: String,
 	Level: String,
 	CourseOfStudy: String,
-	Complaints: [mongoose.Schema(ComplaintSchema)]
+	Complaints: [{ type: mongoose.Schema.Types.ObjectId, ref: "Complaint" }]
 };
 
 const Student = mongoose.model("Student", StudentSchema);
-
-// Validation function
 
 function ValidateStudent(student) {
 	const schema = {
@@ -29,12 +26,10 @@ function ValidateStudent(student) {
 		MatNo: Joi.string().required(),
 		Department: Joi.string(),
 		Level: Joi.string().required(),
-		CourseOfStudy: Joi.string().required(),
-		Complaints: Joi.string().required()
+		CourseOfStudy: Joi.string().required()
 	};
 
 	return Joi.validate(student, schema);
 }
 
-//Exports
 module.exports = { Student, StudentSchema, ValidateStudent };
