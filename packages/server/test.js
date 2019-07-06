6572020f2e0709ade298afd589019a1a90defd02
Torsxperience <Torsxperience@outlook.com>
2019-07-06 22:31:32 +0100

    Tested some code logic before implementation

--- packages/server/test.js
+++ packages/server/test.js
@@ -1,11 +1,8 @@
 const _ = require("lodash");
 
-let a = [{ type: 1, value: 55 }, { type: 2, value: 55 }, { type: 3, value: 55 }];
+function test(val1,val2 = "tems") {
+    console.log(val2)
+}
 
-let b = a.filter(element => element.type === 2);
-
-//console.log(a[_.random((a.length - 1),0,false)]);
-
-// let c = [1];
-// console.log(c.length);
-
+test(1);
+test(2, "segun");
